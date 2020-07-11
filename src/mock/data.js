c43e6c2f38cbc397bda0032e19d72e967b6c9e11
Arnold Kenaci <arnold.kenaci1@gmail.com>
2020-07-11 23:37:23 +0200

    Initial commit

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,16 +2,16 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'Arnold Kenaci Portofolio', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
+  description: 'Welcome to my website', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
+  title: 'Portofolio',
+  name: 'Arnold Kenaci',
+  subtitle: 'I am a Full Stack Engineer',
   cta: '',
 };
 
